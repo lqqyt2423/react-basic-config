d8e626aab2e7f8719f65429c9dd79da12efb4003
liqiang <974923609@qq.com>
2018-01-29 17:03:22 +0800

    first

---
+++ app/index.jsx
@@ -0,0 +1,28 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('app')
+);
